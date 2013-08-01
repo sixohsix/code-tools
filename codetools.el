a469531cd12324374451caefcb10f465a754072b
Mike Verdone <mike.verdone@gmail.com>
2013-08-01 14:15:09 +0200

    Update emacs file.

--- codetools.el
+++ codetools.el
@@ -2,11 +2,19 @@
 ;; Copy and paste this into your .emacs.
 
 
-;; Bind "C-c i" to reorder imports.
-(defun reorder-imports (&optional b e)
+(defun sh-region-replace (command &optional b e)
   (interactive "r")
-  (push-mark)
-  (shell-command-on-region b e "reorder_imports" (current-buffer) 't)
-  (pop-mark)
+  (shell-command-on-region b e command (current-buffer) 't)
   )
-(global-set-key (read-kbd-macro "C-c i") 'reorder-imports)
+
+;; Bind "C-c i" to reorder imports.
+(global-set-key
+ (read-kbd-macro "C-c i")
+ (lambda (&optional b e) (interactive "r")
+   (sh-region-replace "reorder_imports2" b e)))
+
+;; Bind "C-c a" to alphabetize.
+(global-set-key
+ (read-kbd-macro "C-c a")
+ (lambda (&optional b e) (interactive "r")
+   (sh-region-replace "pyalphabetize" b e)))
